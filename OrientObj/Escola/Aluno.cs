b014a582c4f4aa7754839e575c7d2d56773f71e6
Thiago Kasper de Souza <thiagokasper101@gmail.com>
2023-01-30 01:33:53 -0300

    #4-release: iniciando projeto de orientação a objeto

---
+++ OrientObj/Escola/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrientObj.Escola
+{
+    public class Aluno
+    {
+        public string Nome { get; }
+        public int Matricula { get; }
+        public decimal Media { get; }
+
+        public Aluno(string name, int matricula, decimal media)
+        {
+            Nome = name;
+            Matricula = matricula;
+            Media = media;
+        }
+
+        public void MostraInfo()
+        {
+            Console.WriteLine($"Nome: {Nome}\nMatrícula: {Matricula}\nMédia: {Media}");
+        }
+    }
+}
